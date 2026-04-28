e087749b11ddbcee0dd568b74ba3764ee02cfc7c
Fernando <fernanguedezr@gmail.com>
2026-04-28 18:54:58 +0200

    Update src/pages/MyJob.jsx

--- src/pages/MyJob.jsx
+++ src/pages/MyJob.jsx
@@ -1,1 +1,12 @@
-import 
+import Button from "../components/Button";
+
+const MyJob = () => {
+  return (
+    <div>
+      <h1>Esta es la página MyJobs</h1>
+      <Button />
+    </div>
+  );
+};
+
+export default MyJob;
